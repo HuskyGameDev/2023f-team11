bbb3af3433dc87c2fe560dd559a4bb55ed7dcc13
Nick Abraham <nrabraha@mtu.edu>
2023-10-30 18:19:53 -0400

    Locked background
    Locked the background so it follows the player up the platforms

--- Blizzards Climb/Assets/Scripts/CameraControl.cs
+++ Blizzards Climb/Assets/Scripts/CameraControl.cs
@@ -1,39 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public Transform target;
-    //public Transform background1;
-    //public Transform background2;
-    //private float size;
+    public Transform playerTarget;
+    public Transform backgroundTarget;
+    public Transform cameraObject;
+    public GameObject background;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //size = background1.GetComponent<BoxCollider2D>().size.y;
+        background.transform.SetParent(cameraObject);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //camera
-        Vector3 targetPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
-
-        //background
-        //if (transform.position.y >= background2.position.y)
-        //{
-        //    background1.position = new Vector3(background1.position.x, background2.position.y + size, background1.position.z);
-        //    SwitchBackground();
-        //}
+        Vector3 playerTargetPos = new Vector3(transform.position.x, playerTarget.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, playerTargetPos, 0.2f);
     }
-
-    //private void SwitchBackground()
-    //{
-    //    Transform temp = background1;
-    //    background1 = background2;
-    //    background2 = temp;
-    //}
 }
