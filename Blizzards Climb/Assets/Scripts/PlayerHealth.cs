e2c7dd2133a37e203ca758bda16ceeffb5010f10
GitHupee <buckwalter59@gmail.com>
2023-11-09 19:37:30 -0500

    Cafe Melt and Tree Hazard
    Implemented tree hazard which damages player on collision as well as cafe melt item which damages player when passing through.

--- Blizzards Climb/Assets/Scripts/PlayerHealth.cs
+++ Blizzards Climb/Assets/Scripts/PlayerHealth.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour 
 {
 	public int health;
+	public int maxHealth = 10;
+
+	void Start()
+	{
+		health = maxHealth;
+	}
 
 	public void TakeDamage(int damage)
 	{
