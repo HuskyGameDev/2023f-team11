a7e883818df988265bb5852ece0f6f4bed43d4bb
ndbrewer <ndbrewer@mtu.edu>
2024-01-17 22:06:14 -0500

    Movement (jumping mostly) + prefab
    Lots changed but to summarize: Changed up some movement controls (using a temporary new input system till I get the final one) and changed how jumping works to allow a grace period after leaving the platform that you can still jump also included jump buffering which lets you queue a jump, Also gave feedback to the player for stomping on an enemy that throws the player upwards Mario style, ground detection was also buggy and sometimes the player would get stuck on a platform even though they were still on it so i reworked the ground detection system to use a Box Cast instead of Overlap Circle to fully check the length of the players bounding box, fixed the player getting randomly stopped by ghost vertices messing with collision using tilemap collider [used by composite] + Composite Collider 2D on tilemap. also created an example on the testlevel scene to add more space exaggeration for jumps to show changes. added a new folder to the assets folder called "Prefabs" Inside is a "Player" Folder that currently only has the Player prefab. any prefabs related to the player should be stored in this folder also.

--- Blizzards Climb/Assets/Scripts/Player Scripts/BlizzardMovement.cs
+++ Blizzards Climb/Assets/Scripts/Player Scripts/BlizzardMovement.cs
@@ -1,77 +1,235 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-
+/// <summary>
+/// much inspiration comes from tarodevs video on 2D platformer controller but instead of using custom physics it just uses rigidbody2D <br/>
+/// https://www.youtube.com/watch?v=3sWTzMsmdx8
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class BlizzardMovement : MonoBehaviour
 {
+    [Header("Movement")]
+    public float speed = 8f; // speed of the player
+    public float GroundDeceleration = 60;
+    public float AirSpeed = 4f;
+    public float AirDeceleration = 120;
     private float horizontal; // player location of x axis
-    private float speed = 8f; // speed of the player
-    private float jumpingPower = 16f; // jumping power of the player
-    private bool facingRight = true; // determines if player if facing left or right
 
 
-    [SerializeField] private Rigidbody2D rb; // player's rigid body
-    [SerializeField] private Transform groundCheck; // checks if player is on the ground
+    [Header("Jumping")]
+    public float jumpingPower = 16f; // jumping power of the player
+    private bool jump;
+    public float jumpBuffer = .2f;
+    private bool bufferJumpUsable;
+    private bool canBufferJump => bufferJumpUsable && time < lastJumpPressed + jumpBuffer;
+
+    [Tooltip("how long after leaving an edge can you jump?")]
+    public float CoyoteTime = .15f;
+    private bool coyoteUsable; // can you jump after leaving solid ground?
+    private bool canUseCoyote => coyoteUsable && !grounded && time < lastTimeOnGround + CoyoteTime; // are you able to use coyote time?
+
+    private float lastJumpPressed;
+
+
+    [Header("Ground Detection")]
+    [SerializeField] private Vector2 GPosLocal;
+    private Vector2 GPosGlobal => (Vector2)transform.position + GPosLocal;
+    private bool grounded;
+    [SerializeField] private Vector2 GPosSize;
+    //[SerializeField] private float GroundCheckRadius;
     [SerializeField] private LayerMask groundLayer; // the gorund layer 
-    [SerializeField] private Transform iceCheck;
-    [SerializeField] private LayerMask iceLayer;
+    private float lastTimeOnGround = float.MinValue; // when was the last time you touched the ground? 
 
-   void Update()
+    //[SerializeField] private Transform iceCheck;
+    //[SerializeField] private LayerMask iceLayer;
+
+
+    [Header("References")]
+    [SerializeField, Tooltip("If no renderer is set then it will search the gameobject attached to the script for a renderer.")]
+    private SpriteRenderer spriteRenderer;
+    //private bool facingRight = true; // determines if player if facing left or right
+    public Rigidbody2D rb; // player's rigid body
+
+    // good for keeping track of how long it's been since something has happened or generally just to time buffers or jumps.
+    private float time; // time since the controller has started.
+
+    #region Player Controls (_PC)
+    private PlayerControls _PC; // use the new input system for player control so you can easily allow for interchange between arrow keys, wasd, or gamepad
+    private void OnEnable()
     {
-        horizontal = Input.GetAxisRaw("Horizontal"); // instantiates the horizontal by getting which direction the player is moving
-       
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        { // if the jump button is pushed and the player is grounded
-        // the player's value on the y axis is set to the jumping power
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        _PC.Enable();
+    }
+    private void OnDisable()
+    {
+        _PC.Disable();
+    }
+    #endregion
+
+    // Called when an enabled script instance is being loaded.
+    private void Awake()
+    {
+        _PC = new PlayerControls();
+
+        if (!rb) // rigidbody is null
+            rb = GetComponent<Rigidbody2D>(); // find something
+
+        if (!spriteRenderer) // sprite renderer is null
+            spriteRenderer = GetComponent<SpriteRenderer>(); // find something
+    }
+
+    private void GetInput()
+    {
+        // instantiates the horizontal by getting which direction the player is moving
+        horizontal = _PC.Movement.Horizontal.ReadValue<float>();
+
+        // check if player is trying to jump.
+        if (_PC.Movement.Jump.WasPressedThisFrame())
+        {
+            // now is the last time you pressed jump.
+            lastJumpPressed = time;
+            // you are trying to jump now.
+            jump = true;
         }
+    }
 
+    void Update()
+    {
+        // What is trying to happen here?
+        // I can only assume you are trying to induce slide to the player on ice.
+        // But i believe you can just use physics materials to reduce friction on objects?
+        // - Nathan
+        //   if (IsGrounded() && IsIcey())
+        //{
+        //    rb.velocity = new Vector2(4f, jumpingPower);
+        //}
 
-        if (IsGrounded() && IsIcey())
-	    {
-	        rb.velocity = new Vector2(4f, jumpingPower);
-	    }
+        // What is trying to happen here?
+        // if you are trying to simulate gravity the rigid body is already doing that.
+        // also if you are trying to tell when the player has released the "jump" button it would be GetButtonUp("Jump")
+        // - Nathan
+        //if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
+        //{ // if the player is in the air, this takes the player gradually 
+        //  // back down once the jump button is released
+        //  //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        //}
 
-        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
-        { // if the player is in the air, this takes the player gradually 
-        // back down once the jump button is released
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
+        time += Time.deltaTime; // get the time before anything is done because we are on a new frame.
+        GetInput();
 
-
+        // flip the player after getting input.
         Flip();
     }
 
-
     private void FixedUpdate()
-    { // applies the speed and direction to the rigidbody of the player
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    {
+        // collision checks
+        checkCollision();
+
+        // handle external things like jumping.
+        HandleJump();
+
+        // move the player after everything else has been dealt with.
+        MovePlayer();
     }
 
-    private bool IsIcey()
+    private void MovePlayer()
     {
-        return Physics2D.OverlapCircle(iceCheck.position, 0.2f, iceLayer);
-    } 
-    
-    private bool IsGrounded()
+        // applies the speed and direction to the rigidbody of the player
+        if (grounded)
+            rb.AddForce(Vector2.right * horizontal * speed, ForceMode2D.Force);
+        else // if you are airborne use an airspeed (horizontal) multiplier instead of normal speed.
+            rb.AddForce(Vector2.right * horizontal * AirSpeed, ForceMode2D.Force);
+
+        // lets you maintain some acceleration while keeping controls snappy.
+        var deceleration = grounded ? GroundDeceleration : AirDeceleration;
+        if (horizontal == 0) rb.velocity = new Vector2(Mathf.MoveTowards(rb.velocity.x, 0, deceleration * Time.fixedDeltaTime), rb.velocity.y);
+    }
+
+    private void HandleJump()
+    {
+        // if you don't have the ability to jump (either you haven't pressed the jump button yet or you don't have a buffer jump ready
+        // just exit the function early.
+        if (!jump && !canBufferJump) return;
+
+        // if you are grounded or can use the coyote jump then execute a jump.
+        if (grounded || canUseCoyote) Jump();
+
+        // you no longer want to jump after jumping.
+        jump = false;
+    }
+
+    private void Jump()
+    {
+        // you can no longer use coyote after jumping.
+        coyoteUsable = false;
+
+        // can't buffer your jump while starting a jump.
+        bufferJumpUsable = false;
+
+        // don't want to use an old jump time so reset
+        lastJumpPressed = 0;
+
+        // reset the y velocity.
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+
+        // add the jump force to the player
+        rb.AddForce(Vector2.up * jumpingPower, ForceMode2D.Impulse);
+    }
+
+    //private bool IsIcey()
+    //{
+    //    return Physics2D.OverlapCircle(iceCheck.position, 0.2f, iceLayer);
+    //}
+
+    private void checkCollision()
     { // checks to see if the player is standing on the ground by creating
-        // an invisible circle at the players feet. When this circle touches
-        // the ground layer, then the play can jump.
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+      // an invisible circle at the players feet. When this circle touches
+      // the ground layer, then the play can jump.
+
+        // use a box cast to match the x dimension of the player bounding box to prevent player from getting stuck on edge of tilemap.
+        bool groundHit = Physics2D.BoxCast(GPosGlobal, GPosSize, 0, Vector2.down, GPosSize.y, groundLayer);
+
+        // if previous state was not on the ground but we detect ground
+        if (!grounded && groundHit)
+        {
+            grounded = true; // we are grounded ;)
+            bufferJumpUsable = true; // we can buffer a jump again.
+            coyoteUsable = true; // and we can use coyote jump again.
+        }
+        // if we are previously grounded but no longer see ground
+        else if (grounded && !groundHit)
+        {
+            grounded = false; // we are no longer grounded.
+            lastTimeOnGround = time; // and the last time we saw the ground was now.
+        }
+        //return Physics2D.OverlapCircle(GPosGlobal, GroundCheckRadius, groundLayer);
     }
 
     private void Flip()
-    { // flips the sprite depending on which direction the player is facing.
-        if (facingRight && horizontal < 0f || !facingRight && horizontal > 0f)
-        {
-            facingRight = !facingRight; // player is facing left
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
+    {
+        // there is already an easy way to flip the direction of the player using the sprite renderer - Nathan
+        // flips the sprite depending on which direction the player is facing.
+        //if (facingRight && horizontal < 0f || !facingRight && horizontal > 0f)
+        //{
+        //    facingRight = !facingRight; // player is facing left
+        //    Vector3 localScale = transform.localScale;
+        //    localScale.x *= -1f;
+        //    transform.localScale = localScale;
+        //}
 
+        // if looking left flip to look left.
+        if (horizontal < 0) spriteRenderer.flipX = true;
+        // else just look forward normally.
+        // if we want it to look the direction that was last moved just change to "else if (horizontal > 0) spriteRenderer.flipX = false;"
+        else spriteRenderer.flipX = false;
+    }
 
-        }
+    // when we select the gameobject attached to this script draw gizmos.
+    private void OnDrawGizmosSelected()
+    {
+        // set the color so we can see the gizmo from the background.
+        Gizmos.color = Color.red;
+        // draw a cube to represent the ground position checker.
+        Gizmos.DrawWireCube(GPosGlobal, GPosSize);
     }
 
     /*private void OnCollisionEnter2D(Collision2D collider) {
