046671221db42c9bb4873b8754f9cada1dd3d19b
OneAsterisk <90720963+OneAsterisk@users.noreply.github.com>
2024-01-18 21:50:52 -0500

    Updated to New Input System
    Update to the new modern unity input system.
    Modified movement and jumping to work with this system.

--- Blizzards Climb/Assets/Scripts/Player Scripts/BlizzardMovement.cs
+++ Blizzards Climb/Assets/Scripts/Player Scripts/BlizzardMovement.cs
@@ -1,59 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 public class BlizzardMovement : MonoBehaviour
 {
-    private float horizontal; // player location of x axis
-    private float speed = 8f; // speed of the player
-    private float jumpingPower = 16f; // jumping power of the player
+    private CustomInput playerInput; //Init the customInput Variable as input
+    private float horizontalInput; // player location of x axis
     private bool facingRight = true; // determines if player if facing left or right
+    private InputAction move = null; // InputAction for movement
+    private InputAction jump = null; // InputAction for jumping
+    private Vector2 moveDirection = Vector2.zero;
 
-
-    [SerializeField] private Rigidbody2D rb; // player's rigid body
+    [SerializeField] private float speed; // speed of the player
+    [SerializeField] private float jumpingPower; // jumping power of the player
+    [SerializeField] private Rigidbody2D player; // player's rigid body
     [SerializeField] private Transform groundCheck; // checks if player is on the ground
-    [SerializeField] private LayerMask groundLayer; // the gorund layer 
-    [SerializeField] private Transform iceCheck;
-    [SerializeField] private LayerMask iceLayer;
+    [SerializeField] private LayerMask groundLayer; // the ground layer
+    //[SerializeField] private Transform iceCheck;  // Checks if the player is on ice
+    [SerializeField] private LayerMask iceLayer;  // Gets the iceLayer
 
-   void Update()
+
+
+    // Assigns the playerInput the customInput variable on wake
+    private void Awake()
     {
-        horizontal = Input.GetAxisRaw("Horizontal"); // instantiates the horizontal by getting which direction the player is moving
+        playerInput = new CustomInput();
+    }
+
+    private void OnEnable()
+    {
+        move = playerInput.Player.Movement;
+        jump = playerInput.Player.Jump;
+        move.Enable();
+        jump.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        jump.Disable();
+    }
+
+
+    void Update()
+    {
+        #region inputs
+        moveDirection = move.ReadValue<Vector2>();
+        horizontalInput = moveDirection.x;
+        #endregion
+        //New jump check uses the new input system to check if the button assigned to jump is pressed
+        //And that the player is grounded
        
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        { // if the jump button is pushed and the player is grounded
-        // the player's value on the y axis is set to the jumping power
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        if(jump.IsPressed() && IsGrounded())
+        {
+            Jump();
         }
-
-
+        /* IsIcey is not yet properly implemented
         if (IsGrounded() && IsIcey())
 	    {
-	        rb.velocity = new Vector2(4f, jumpingPower);
+	        player.velocity = new Vector2(4f, jumpingPower);
 	    }
-
-        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
-        { // if the player is in the air, this takes the player gradually 
-        // back down once the jump button is released
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-
-
+        */
+        //Check which way the player is moving && facing and then call flip() to flip the sprite
         Flip();
     }
 
-
+    private void Jump()
+    {
+        #region Do the Jump
+        player.velocity = new Vector2(player.velocity.x, jumpingPower);
+        #endregion
+    }
     private void FixedUpdate()
     { // applies the speed and direction to the rigidbody of the player
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        //Move the character 
+        player.velocity = new Vector2(horizontalInput * speed * Time.deltaTime,player.velocity.y);
     }
+    
 
+    /* is not yet implemented
     private bool IsIcey()
     {
         return Physics2D.OverlapCircle(iceCheck.position, 0.2f, iceLayer);
     } 
-    
+    */
     private bool IsGrounded()
     { // checks to see if the player is standing on the ground by creating
         // an invisible circle at the players feet. When this circle touches
@@ -63,7 +95,7 @@ public class BlizzardMovement : MonoBehaviour
 
     private void Flip()
     { // flips the sprite depending on which direction the player is facing.
-        if (facingRight && horizontal < 0f || !facingRight && horizontal > 0f)
+        if (facingRight && horizontalInput < 0f || !facingRight && horizontalInput > 0f)
         {
             facingRight = !facingRight; // player is facing left
             Vector3 localScale = transform.localScale;
@@ -73,12 +105,12 @@ public class BlizzardMovement : MonoBehaviour
 
         }
     }
-
+    
+    }
     /*private void OnCollisionEnter2D(Collision2D collider) {
          Debug.Log("Collision.");
          if (collider.gameObject.tag == "Enemy") {
              // Do damage
          }
     }*/
-}
 
