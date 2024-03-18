4129ba2a4f0d62d217de17273097931bc7ca473a
Jacob Newman <jjnewman@mtu.edu>
2024-03-18 15:44:14 -0400

    Spawner Changes
    Renamed the skier spawner to enemy spawner and deleted the fast bird spawner. Having multiple scripts was not needed.

---
+++ Blizzards Climb/Assets/Scripts/Enemy Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy; // Sets the game object to the skier
+    private GameObject spawnedEnemy; // The spawned enemy
+    public float interval = 5; // Seconds to wait before spawn
+    private float counter = 0; // Time since last spawn
+    public float deathInterval = 10; // Time unitl death of enemy
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!spawnedEnemy && counter >= interval) {
+            counter = 0;
+            spawnedEnemy = Instantiate(enemy, transform.position, transform.rotation);
+        }
+        else if(spawnedEnemy && counter >= deathInterval) {
+            Destroy(spawnedEnemy);
+            counter = 0;
+        }
+        counter += Time.deltaTime;
+    }
+
+    
+
+}
