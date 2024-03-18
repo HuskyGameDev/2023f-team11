695ed1a7a26bf4d8a8778420c1dc2d2ea6aea80e
OneAsterisk <90720963+OneAsterisk@users.noreply.github.com>
2024-03-17 20:43:36 -0400

    Started Tutorial level and Modified Camera

--- Blizzards Climb/Assets/Scripts/Player Scripts/CameraControl.cs
+++ Blizzards Climb/Assets/Scripts/Player Scripts/CameraControl.cs
@@ -6,6 +6,8 @@ public class CameraControl : MonoBehaviour
     public Transform backgroundTarget;
     public Transform cameraObject;
     public GameObject background;
+    public float leftBound = 0.3f; //Left boundary of the camera as a percent of screen width
+    public float rightBound = 0.7f; //Right boundary of the camera as a percent of screen width
 
     void Start()
     {
@@ -14,7 +16,7 @@ public class CameraControl : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 playerTargetPos = new Vector3(transform.position.x, playerTarget.position.y, transform.position.z);
+        Vector3 playerTargetPos = new Vector3(playerTarget.position.x, playerTarget.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, playerTargetPos, 0.2f);
     }
 }
