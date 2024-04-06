fcab5032d4d20ea0b3a1b06198f43e99fbbbee22
ndbrewer <ndbrewer@mtu.edu>
2024-04-05 20:54:46 -0400

    Cafe Melt + Player
    Cafe Melt:
    *Added Cafe melt healing the player for set amount
    *Amount healed per cafe melt is customizable
    *destroys on use
    
    Player:
    *"privatized" the health and maxhealth variables so they couldn't be changed in other scripts but are still referenceable.
    *added Heal Player Function
    *if the player tries to heal while at full health the Full message will display saying "I'm full".

--- Blizzards Climb/Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Blizzards Climb/Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class PlayerHealth : MonoBehaviour 
 {
-	public int health;
-	public int maxHealth = 5;
+	public int health { get; private set; }
+	public int maxHealth { get; private set; } = 5;
+
+	public TextMeshPro fullHealth_Text;
 
 	void Start()
 	{
@@ -20,9 +24,40 @@ public class PlayerHealth : MonoBehaviour
 		Debug.Log("Health = " + health.ToString());
 	}
 
+	public bool HealPlayer(int healAmount)
+	{
+
+		// player health should not go above max health.
+		if(health+healAmount > maxHealth)
+		{
+            StartCoroutine(DisplayFullMessage());
+            return false;
+        }
+			
+		else
+			health += healAmount;
+        Debug.Log("Health = " + health.ToString());
+
+		return true;
+    }
+
 	private void KillPlayer()
 	{
 		// i mean you don't have to do anything fancy just reload the current scene?
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	IEnumerator DisplayFullMessage()
+	{
+		if (fullHealth_Text)
+		{
+			fullHealth_Text.gameObject.SetActive(true);
+
+			yield return new WaitForSeconds(2);
+
+			fullHealth_Text.gameObject.SetActive(false);
+		}
+		else Debug.LogWarning($"PlayerHealth.cs: fullHealth_text reference not set.");
+		
 	}
 }
